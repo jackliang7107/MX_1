4d2e897cf2998998e88ccd1e02f55c116365ecca
jackliang7107 <44103021+jackliang7107@users.noreply.github.com>
2019-09-24 13:57:34 +1000

    Add files via upload

---
+++ PART C.c
@@ -0,0 +1,47 @@
+/* 
+
+This code continually scans ADC0 for an analog signal, using 16Mhz processor clock
+
+*/
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <myFirstLibrary.h>
+#define Trigvalue 200
+
+
+
+volatile uint8_t ADCvalue;            // Global variable , set to volatile the value of the variable may change. if used with ISR 
+
+
+int main(void)
+{
+
+  ADC_inti();
+  sei();                    
+  pinMode(PINB1,OUTPUT);
+
+  while (1)
+  {
+  }
+}
+
+
+ISR(ADC_vect)
+{
+    ADCvalue = ADCL;                     // only need to read the high value for 8 bit
+    uint16_t theTenBitResults = ADCH<<8 | ADCvalue;
+    
+    if (theTenBitResults > Trigvalue)
+    {
+      digitalWrite(PINB1,HIGH);  
+    }
+    else 
+    {
+      //digitalWrite(PINB1,LOW);
+      PORTB &= ~(1<<PINB1);
+    }                                    
+}
+
+
